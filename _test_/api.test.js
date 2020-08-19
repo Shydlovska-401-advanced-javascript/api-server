9d21719abb39ea8594d6addf5bf1eeb07a1ad76b
Kateryna Shydlovska <shydlovskayaka@gmail.com>
2020-08-19 11:16:40 -0700

    added some tests

--- _test_/api.test.js
+++ _test_/api.test.js
@@ -4,39 +4,39 @@ const { server } = require('../lib/server.js');
 const supertest = require('supertest');
 const mockRequest = supertest(server);
 
-describe('web server', () => {
+describe('middleware', () => {
 
 
-  // it('should respond with a 404 on an invalid route', () => {
+  it('should respond with a 404 on an invalid route', () => {
 
-  //   return mockRequest
-  //     .get('/bad')
-  //     .then(results => {
-  //       expect(results).toBe(404);
-  //     }).catch(console.error);
-
-  // });
-
-  // it('should respond with a 404 on an invalid method', () => {
-
-  //   return mockRequest
-  //     .post('/')
-  //     .then(results => {
-  //       expect(results.status).toBe(404);
-  //     }).catch(console.error);
-
-  // });
-
-  it('should respond properly on request to /categories', async () => {
-
-    // return mockRequest
-    const response = await mockRequest
-      .get('/categories');
-        expect(response.status).toBe(200);
-      
+    return mockRequest
+      .get('/bad')
+      .then(results => {
+        expect(results).toBe(404);
+      }).catch(console.error);
 
   });
 
-  // What strategies should we use to test POST, PUT, DELETE?
+  it('should respond with a 404 on an invalid method', () => {
+
+    return mockRequest
+      .post('/')
+      .then(results => {
+        expect(results.status).toBe(404);
+      }).catch(console.error);
+
+  });
+
+  it('should respond properly on request to /categories', async () => {
+
+    return mockRequest
+      .get('/categories')
+      .then(result => {
+      expect(result.status).toBe(200);
+      }).catch(console.error);
+       
+      
+
+  });
 
 });
